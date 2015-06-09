0e385128df196e6659fd057030a84ee8b4d9bc9b
AlexRickert <ros_asrickert@WROSRM31915.seattleschools.org>
2015-06-09 11:39:13 -0700

    Final commit?
    I am not sure if this commit will fail or not. Hope it works! It is just
    kind of everything finally put into one or two places, really just
    everything into ActuallyMain.java If we just change a couple lines of
    code half of it works and if we change it back the other half works.
    It's fine.

--- src/FourPersonPong/TesterForJavaSwing.java
+++ src/FourPersonPong/TesterForJavaSwing.java
@@ -1,37 +1,28 @@
 package FourPersonPong;
 
-import my.contacteditor.ContactEditorUI;
-
 import java.awt.*;
-import java.awt.event.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
-import javax.swing.JCheckBoxMenuItem;
-import javax.swing.JRadioButtonMenuItem;
-import javax.swing.ButtonGroup;
-import javax.swing.JMenuBar;
-import javax.swing.KeyStroke;
-import javax.swing.ImageIcon;
- 
+
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
-import javax.swing.JScrollPane;
 
 final public class TesterForJavaSwing {
 
    JFrame frame;
    DrawPanel drawPanel;
-   private int oneX;
-   private int oneY;
-   
-   //public  color
+   private int BallOneX;
+   private int BallOneY;
 
-   
-   int bob = 1;
+   private int Paddle1x;
+   private int Paddle1Y;
+
+   private int Paddle2x;
+   private int Paddle2Y;
+
+   private int Paddle3x;
+   private int Paddle3Y;
+
+   private int Paddle4x;
+   private int Paddle4Y;
 
    boolean up = true;
    boolean down = false;
@@ -45,13 +36,12 @@ final public class TesterForJavaSwing {
    public void go() {
       frame = new JFrame("IT BOUNCES");
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      
-      TesterForJavaSwing demo = new TesterForJavaSwing();;
+
       drawPanel = new DrawPanel();
    
       frame.getContentPane().add(BorderLayout.CENTER, drawPanel);
       frame.setVisible(true);
-      frame.setResizable(true);
+      frame.setResizable(false);
       frame.setSize(750, 750);
       frame.setLocation(375, 55);
       
@@ -63,27 +53,9 @@ final public class TesterForJavaSwing {
       
          g.setColor(Color.WHITE);
          g.fillRect(6, 6, this.getWidth()-12, this.getHeight()-12);
-         /*
-         if (bob%25 >= 0 && bob%25 <= 5) {
-            g.setColor(Color.RED);
-         } 
-         else if (bob%25 >= 6 && bob%25 <= 10) {
-            g.setColor(Color.BLUE);
-         } 
-         else if (bob%25 >= 11 && bob%25 <= 15) {
-            g.setColor(Color.GREEN);
-         } 
-         else if (bob%25 >= 16 && bob%25 <= 20) {
-            g.setColor(Color.YELLOW);
-         } 
-         else {
-            g.setColor(Color.MAGENTA);
-         }
-         
-         */
-         bob++;
-         g.drawOval(oneX, oneY, 30, 30);
-         g.fillPolygon(new int[]{oneX+15, oneX+24, oneX+2, oneX+28, oneX+6}, new int[]{oneY, oneY+27, oneY+10, oneY+10, oneY+27}, 5);
+         g.setColor(Color.orange);
+         g.drawOval(BallOneX, BallOneY, 30, 30);
+         g.fillPolygon(new int[]{BallOneX +15, BallOneX +24, BallOneX +2, BallOneX +28, BallOneX +6}, new int[]{BallOneY, BallOneY +27, BallOneY +10, BallOneY +10, BallOneY +27}, 5);
       
       }
    }
@@ -92,35 +64,51 @@ final public class TesterForJavaSwing {
       while(true){
 
          //BALL
-         if(oneX >= frame.getSize().getWidth()-40){
+         if(BallOneX >= frame.getSize().getWidth()-40){
             right = false;
             left = true;
          }
-         if(oneX <= 0){
+         if(BallOneX <= 0){
             right = true;
             left = false;
          }
-         if(oneY >= frame.getSize().getHeight()-60){
+         if(BallOneY >= frame.getSize().getHeight()-60){
             up = true;
             down = false;
          }
-         if(oneY <= 0){
+         if(BallOneY <= 0){
             up = false;
             down = true;
          }
          if(up){
-            oneY-=.0035 * frame.getSize().getHeight();
+            BallOneY -=.0035 * frame.getSize().getHeight();
          }
          if(down){
-            oneY+=.0035 * frame.getSize().getHeight();
+            BallOneY +=.0035 * frame.getSize().getHeight();
          }
          if(left){
-            oneX-=.0035 * frame.getSize().getWidth();
+            BallOneX -=.0035 * frame.getSize().getWidth();
          }
          if(right){
-            oneX+=.0035 * frame.getSize().getWidth();
+            BallOneX +=.0035 * frame.getSize().getWidth();
          }
          //BALL
+
+         //PADDLE 1
+
+         //PADDLE 1
+
+         //PADDLE 2
+
+         //PADDLE 2
+
+         //PADDLE 3
+
+         //PADDLE 3
+
+         //PADDLE 4
+
+         //PADDLE 4
 
          try{
             Thread.sleep(10);
--- src/my/contacteditor/ActuallyMain.java
+++ src/my/contacteditor/ActuallyMain.java
@@ -1,15 +1,18 @@
 package my.contacteditor;
 
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 /**
  * Created by ros_asrickert on 5/29/2015.
  */
 
 public class ActuallyMain {
-    private JFrame frame = new JFrame();
-    private JPanel mainPanel = new JPanel();
+    private JFrame frame = new JFrame("Four Person Pong");
+    private DrawPanel dP;
     private ButtonGroup buttonGroup1;
     private JButton jButton1;
     private JCheckBox jCheckBox1;
@@ -19,14 +22,147 @@ public class ActuallyMain {
     private JRadioButton jRadioButton2;
     private JRadioButton jRadioButton3;
     private boolean speed = false;
-    private GroupLayout layout = new GroupLayout(mainPanel);
+    JLayeredPane mainPanel = new JLayeredPane();
+    private boolean red = true;
+    private boolean blue = false;
+    private boolean green = false;
+    private int BallOneX;
+    private int BallOneY;
+
+    private int Paddle1x;
+    private boolean Paddle1Right = true;
+
+    private int Paddle2Y;
+    private boolean Paddle2Up = true;
+
+    private int Paddle3x;
+    private boolean Paddle3Right = true;
+
+    private int Paddle4Y;
+    private boolean Paddle4Up = true;
+
+    boolean up = true;
+    boolean down = false;
+    boolean left = false;
+    boolean right = true;
 
     public static void main (String[] args) {
         new ActuallyMain().run();
     }
 
+
+
+    public void keyPressed(KeyEvent e) {
+        System.out.println(e);
+    }
+
+    private void moveIt() {
+        while(true){
+            //BALL
+            if(BallOneX >= 715){
+                right = false;
+                left = true;
+            }
+            if(BallOneX <= 0){
+                right = true;
+                left = false;
+            }
+            if(BallOneY >= 695){
+                up = true;
+                down = false;
+            }
+            if(BallOneY <= 0){
+                up = false;
+                down = true;
+            }
+            if(up){
+                if(speed) {
+                    BallOneY -= 10;
+                } else {
+                    BallOneY -= 5;
+                }
+            }
+            if(down){
+                if(speed) {
+                    BallOneY += 10;
+                } else {
+                BallOneY += 5;
+                }
+            }
+            if(left){
+                if(speed) {
+                    BallOneX -= 10;
+                } else {
+                    BallOneX -= 5;
+                }
+            }
+            if(right){
+                if(speed) {
+                    BallOneX += 10;
+                } else {
+                    BallOneX += 5;
+                }
+            }
+            //BALL
+
+            //PADDLE 1
+            if(Paddle1x >= 720){
+                Paddle1Right = false;
+            }
+            if(Paddle1x <= 0){
+                Paddle1Right = true;
+            }
+            //PADDLE 1
+
+            //PADDLE 2
+            //ADD IN LATER
+            //PADDLE 2
+
+            //PADDLE 3
+            if(Paddle3x >= 645){
+                Paddle3Right = false;
+            }
+            if(Paddle3x <= 10){
+                Paddle3Right = true;
+            }
+            //PADDLE 3
+
+            //PADDLE 4
+            //ADD IN LATER
+            //PADDLE 4
+
+            try{
+                Thread.sleep(10);
+            }
+            catch (Exception exc){}
+            frame.repaint();
+        }
+    }
+
+    class DrawPanel extends JPanel {
+
+        public void paintComponent(Graphics g) {
+
+            g.setColor(Color.WHITE);
+            g.fillRect(6, 6, this.getWidth()-12, this.getHeight()-12);
+
+            g.setColor(Color.BLACK);
+            g.fillRect(Paddle1x + 200, 30, 175, 20);
+            //ADD PADDLE 2
+            g.fillRect(Paddle3x + 350, 685, 175, 20);
+            //ADD PADDLE 4
+
+            g.setColor(Color.RED);
+
+            g.drawOval(BallOneX, BallOneY, 30, 30);
+            g.fillPolygon(new int[]{BallOneX +15, BallOneX +24, BallOneX +2, BallOneX +28, BallOneX +6}, new int[]{BallOneY, BallOneY +27, BallOneY +10, BallOneY +10, BallOneY +27}, 5);
+
+
+        }
+    }
+
     private void run() {
-        mainPanel.setLayout(new GridLayout());
+        mainPanel.setLayout(null);
         buttonGroup1 = new ButtonGroup();
         jButton1 = new JButton();
         jRadioButton1 = new JRadioButton();
@@ -45,12 +181,27 @@ public class ActuallyMain {
 
         buttonGroup1.add(jRadioButton1);
         jRadioButton1.setText("Red Ball");
+        jRadioButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jRadioButton1ActionPerformed(evt);
+            }
+        });
 
         buttonGroup1.add(jRadioButton2);
         jRadioButton2.setText("Green Ball");
+        jRadioButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jRadioButton2ActionPerformed(evt);
+            }
+        });
 
         buttonGroup1.add(jRadioButton3);
         jRadioButton3.setText("Blue Ball");
+        jRadioButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jRadioButton3ActionPerformed(evt);
+            }
+        });
 
         jCheckBox1.setText("Speed Run");
         jCheckBox1.addActionListener(new java.awt.event.ActionListener() {
@@ -63,56 +214,87 @@ public class ActuallyMain {
 
         jLabel2.setText("Settings");
 
-        layout.setHorizontalGroup(
-                        (layout.createSequentialGroup()
-                                .addContainerGap()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                        .addGroup(layout.createSequentialGroup()
-                                                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 141, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                                .addGap(112, 112, 112)
-                                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                                        .addComponent(jLabel2))
-                                                        .addComponent(jRadioButton2)
-                                                        .addComponent(jRadioButton1)
-                                                        .addComponent(jRadioButton3)
-                                                        .addComponent(jCheckBox1)
-                                                .addContainerGap(39, Short.MAX_VALUE))
-                                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                                .addGap(0, 0, Short.MAX_VALUE)
-                                                .addComponent(jLabel1)
-                                                .addGap(148, 148, 148))))
-        );
-        layout.setVerticalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(layout.createSequentialGroup()
-                                .addContainerGap()
-                                .addComponent(jLabel1)
-                                .addGap(39, 39, 39)
-                                .addComponent(jLabel2)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(jCheckBox1)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                        .addGroup(layout.createSequentialGroup()
-                                                .addComponent(jRadioButton1)
-                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                                .addComponent(jRadioButton2)
-                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                                .addComponent(jRadioButton3))
-                                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addContainerGap(86, Short.MAX_VALUE))
-        );
+        mainPanel.setMaximumSize(new Dimension(1280, 1024));
 
-        frame.add(mainPanel);
+    //ADDING SHIT TO THE THING
 
+        jLabel1.setBounds(300,0,100,50);
+        mainPanel.add(jLabel1,0);
+
+        jLabel2.setBounds(0,0,75,50 );
+        mainPanel.add(jLabel2,0);
+
+        jRadioButton1.setBounds(0,40,100,40);
+        jRadioButton1.setBackground(Color.red);
+        mainPanel.add(jRadioButton1,0);
+
+        jRadioButton2.setBounds(0,80,100,40);
+        jRadioButton2.setBackground(Color.green);
+        mainPanel.add(jRadioButton2,0);
+
+        jRadioButton3.setBounds(0,120,100,40);
+        jRadioButton3.setBackground(Color.cyan);
+        mainPanel.add(jRadioButton3,0);
+
+        jCheckBox1.setBounds(0,160,100,40);
+        jCheckBox1.setBackground(Color.ORANGE);
+        mainPanel.add(jCheckBox1,0);
+
+        //mainPanel.addKeyListener(KeyListener);
+
+        jButton1.setBounds(175,125,500,500);
+        mainPanel.add(jButton1,0);
+        mainPanel.setVisible(true);
+        frame.add(mainPanel, 1);
+        dP = new DrawPanel();
+        dP.setVisible(false);
+        dP.setBackground(Color.WHITE);
+        frame.add(dP, 0);
         frame.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         frame.setSize(750, 750);
         frame.setVisible(true);
+        frame.setResizable(false);
+
+        moveIt();
 
     }
+
+    private void jRadioButton1ActionPerformed(java.awt.event.ActionEvent evt) {
+        green = false;
+        blue = false;
+        red = true;
+        System.out.println("red");
+    }
+
+    private void jRadioButton2ActionPerformed(java.awt.event.ActionEvent evt) {
+        green = true;
+        blue = false;
+        red = false;
+        System.out.println("green");
+    }
+
+    private void jRadioButton3ActionPerformed(java.awt.event.ActionEvent evt) {
+        green = false;
+        blue = true;
+        red = false;
+        System.out.println("blue");
+    }
+
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {
         // TODO add your handling code here:
          System.out.println("What up");
+
+        jButton1.setVisible(false);
+        jRadioButton1.setVisible(false);
+        jRadioButton2.setVisible(false);
+        jRadioButton3.setVisible(false);
+        jLabel1.setVisible(false);
+        jLabel2.setVisible(false);
+        jCheckBox1.setVisible(false);
+        dP.setVisible(true);
+
+
+
 
         //new FourPersonPong.TesterForJavaSwing().go();
     }
